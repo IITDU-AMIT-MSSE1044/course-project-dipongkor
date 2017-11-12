8026277970109444ad8cc52c45fb547fe7826a3d
Atish Kumar Dipongkor <atishiitdu@hotmail.com>
2017-11-12 19:23:07 +0600

    mior changes

--- Msse.Sa/SearchHelper/Program.cs
+++ Msse.Sa/SearchHelper/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using Nager.PublicSuffix;
 using Newtonsoft.Json;
+using SearchHelper.Models.Google;
 
 namespace SearchHelper
 {
@@ -12,38 +13,80 @@ namespace SearchHelper
 
         class Program
         {
+            delegate bool IsMissing(HtmlResult result, List<HtmlResult> results);
 
+            delegate string GetFollowUpQuery(string query, HtmlResult result);
             static void Main(string[] args)
             {
                 var data = File.ReadAllText("data.json");
-
                 var dataAsDictionary = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(data);
-                var googleSearchHelper = new GoogleSearchHelper();
-                var domainParser = new DomainParser(new WebTldRuleProvider());
-                var totalMRSite = 0;
-                foreach (var query in dataAsDictionary["en"])
+                MrSite(dataAsDictionary);
+                //MrTitle(dataAsDictionary);
+                Console.ReadKey();
+            }
+
+            static void MrSite(Dictionary<string, List<string>> dataAsDictionary)
+            {
+                IsMissing mrSiteMissingDelegate = (htmlResult, htmlResults) =>
                 {
-                    var originalQueryResult = googleSearchHelper.GetSearchResults(query).GetAwaiter().GetResult();
+                    return !htmlResults.Any(item =>
+                        item.Url.Host == htmlResult.Url.Host && item.Title == htmlResult.Title);
+                };
 
-                    foreach (var originalQueryResultItem in originalQueryResult.Items)
+                GetFollowUpQuery mrSiteGetFollowUpQueryDelegate = (query, htmlResult) =>
+                {
+                    var domainParser = new DomainParser(new WebTldRuleProvider());
+                    var domain = domainParser.Get(htmlResult.Url.Host);
+                    var followUpQuery = $"{query} site:.{domain.TLD}";
+                    return followUpQuery;
+                };
+
+                //MRSite (English Google, English Query)
+
+                var mrSiteEnglishGoogleEnglishQuery = CalculateRocof("en-us", dataAsDictionary["en-us"], mrSiteGetFollowUpQueryDelegate, mrSiteMissingDelegate);
+                File.WriteAllText("mrSiteEnglishGoogleEnglishQuery.txt", $"MRSite (English Google, English Query) = {mrSiteEnglishGoogleEnglishQuery}");
+                Console.WriteLine($"MRSite (English Google, English Query) = {mrSiteEnglishGoogleEnglishQuery}");
+
+                ////MRSite (English Google, Chinese Query)
+                //var mrSiteEnglishGoogleChineseQuery = CalculateRocof("en-us", dataAsDictionary["zh-CN"],
+                //    mrSiteGetFollowUpQueryDelegate, mrSiteMissingDelegate);
+                //File.WriteAllText("mrSiteEnglishGoogleChineseQuery.txt", $"MRSite (English Google, Chinese Query) = {mrSiteEnglishGoogleChineseQuery}");
+                //Console.WriteLine($"MRSite (English Google, Chinese Query) = {mrSiteEnglishGoogleChineseQuery}");
+
+
+                ////MRSite (Chinese Google, English Query)
+
+                //var mrSiteChineseGoogleEnglishQuery = CalculateRocof("zh-CN", dataAsDictionary["en-us"], mrSiteGetFollowUpQueryDelegate, mrSiteMissingDelegate);
+                //File.WriteAllText("mrSiteChineseGoogleEnglishQuery.txt", $"MRSite (Chinese Google, English Query) = {mrSiteChineseGoogleEnglishQuery}");
+                //Console.WriteLine($"MRSite (Chinese Google, English Query) = {mrSiteChineseGoogleEnglishQuery}");
+
+                ////MRSite (Chinese Google, Chinese Query)
+                //var mrSiteChineseGoogleChineseQuery = CalculateRocof("zh-CN", dataAsDictionary["zh-CN"],
+                //    mrSiteGetFollowUpQueryDelegate, mrSiteMissingDelegate);
+                //File.WriteAllText("mrSiteChineseGoogleChineseQuery.txt", $"MRSite (Chinese Google, Chinese Query) = {mrSiteChineseGoogleChineseQuery}");
+                //Console.WriteLine($"MRSite (Chinese Google, Chinese Query) = {mrSiteChineseGoogleChineseQuery}");
+            }
+
+            static double CalculateRocof(string hl, List<string> queries, GetFollowUpQuery getFollowUpQuery,
+                IsMissing isMissing)
+            {
+                var totalMrSite = 0;
+                var googleSearchHelper = new GoogleSearchHelper(hl);
+                foreach (var query in queries)
+                {
+                    var originalQueryResults = googleSearchHelper.GetHtmlResult(query)
+                        .Where(m=>m.Url.IsAbsoluteUri);
+                    foreach (var originalQueryResult in originalQueryResults)
                     {
-                        var domainName = domainParser.Get(originalQueryResultItem.DisplayLink);
-                        var followUpQuery = $"{query} site:.{domainName.TLD}";
-
-                        var followUpQueryResult = googleSearchHelper.GetSearchResults(followUpQuery).GetAwaiter().GetResult();
-
-                        if (
-                            !followUpQueryResult.Items.Any(
-                                item =>
-                                    item.Title == originalQueryResultItem.Title &&
-                                    item.Link == originalQueryResultItem.Link))
+                        var followUpQueryResults =
+                            googleSearchHelper.GetHtmlResult(getFollowUpQuery(query, originalQueryResult));
+                        if (isMissing(originalQueryResult, followUpQueryResults))
                         {
-                            totalMRSite++;
+                            totalMrSite++;
                         }
                     }
                 }
-                Console.WriteLine(totalMRSite);
-                Console.ReadKey();
+                return (double)totalMrSite / (queries.Count * 10);
             }
         }
     }
